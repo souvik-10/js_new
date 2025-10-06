f741da9023454c4fd161831a47175e9b38b2ffc5
souvik-10 <souvikk279@gmail.com>
2025-10-06 17:56:22 +0530

    reduce method

--- js/05_iterations/eight.js
+++ js/05_iterations/eight.js
@@ -2,21 +2,44 @@
 
 const myNums = [1, 2, 3]
 
-const myTotal = myNums.reduce(function (acc, currval){
-    console.log(`acc: ${acc} and currval: ${currval}`);
-    return acc + currval
+// const myTotal = myNums.reduce(function (acc, currval){
+//     console.log(`acc: ${acc} and currval: ${currval}`);
+//     return acc + currval
     
-}, 0)
+// }, 0)
 
-console.log(myTotal);
+// reduce method using arrow function (another easy way of writing reduce method)
 
+const myTotal = myNums.reduce( (acc, currval) => acc + currval, 0)
 
-
-
-
-
+// console.log(myTotal);
 
 
+// shopping cart example with reduce method--
+
+const shoppingCart = [
+    {
+        iteName : "js course",
+        price: 3999
+    },
+    {
+        iteName : "py course",
+        price: 1299
+    },
+    {
+        iteName : "app dev course",
+        price: 5999
+    },
+    {
+        iteName : "data ana course",
+        price: 12999
+    },
+    
+]
+
+const priceToPay = shoppingCart.reduce ( (acc, item) => acc + item.price , 0)
+console.log(priceToPay);
+ 
 
 
 
